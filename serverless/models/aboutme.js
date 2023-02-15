69e7c61cc7d24be08e02c912345f94e94690a1ea
Donald Abua <abuadonald@gmail.com>
2023-02-15 19:40:36 +0100

    api_endpoints

--- serverless/models/aboutme.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = new mongoose.Schema({
-	aboutme: String,
-	toolChain: [String],
-	photo: String,
-});
-
-module.exports = mongoose.models.About || mongoose.model("About", schema);
--- utils/mock.js
+++ utils/mock.js
@@ -68,47 +68,37 @@ export const others = [
 export const experiences = [
 	{
 		id: "tab-1",
-		work: "Google",
-		role: "FullStack",
-		date: { start: "Dec 2022", end: "Present" },
-		contribution: [
-			"Write modern, performant, maintainable code for a diverse array of client and internal projects",
-			"Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Gatsby, React, Craft, WordPress, Prismic, and Netlify",
-			"Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
+		work: "Oluadepe Farms",
+		role: "Front-end Developer (Intern)",
+		date: { start: "Jan 2023", end: "Present" },
+		contributions: [
+			"Wrote modern, performant, maintainable code for internal projects",
+			"Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, Electron, Node, React, Netlify",
+			"Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a weekly basis",
+			"Employed an iterative and incremental approach to project management and software development that focuses on delivering value in a flexible and adaptable manner.",
 		],
 	},
 	{
 		id: "tab-2",
-		work: "Microsoft",
-		role: "React",
-		date: { start: "January 2021", end: "April 2022" },
-		contribution: [
-			"Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating from Northeastern",
-			"Helped solidify a brand direction for blistabloc that spans both packaging and web",
-			"Interfaced with clients on a weekly basis, providing technological expertise",
+		work: "Flexisaf",
+		role: "Front-end Developer (Intern)",
+		date: { start: "May 2022", end: "Dec 2022" },
+		contributions: [
+			"Worked with a team of UI/UX designers to build record mangement application for a state polythenic",
+			"With the help of our application they were able to manage about 10,000 students' records in every session and produce their results in time and ensure high integrity of records",
+			"Interfaced with our clients on a weekly basis, providing technological expertise",
 		],
 	},
 	{
 		id: "tab-3",
-		work: "Nulo",
+		work: "Nulo (Personal Project)",
 		role: "Fullstack",
-		date: { start: "January 2021", end: "April 2022" },
-		contribution: [
-			"Developed and shipped highly interactive web applications for Apple Music using Ember.js",
-			"Built and shipped the Apple Music Extension within Facebook Messenger leveraging third-party and internal APIs",
+		date: { start: "August 2022", end: "present" },
+		contributions: [
+			"Developed and shipped highly interactive web UI using Next  JS",
+			"Implemented using a serverless architecture to leverage cloud infrastructure to run its backend logic which includes, scale automatically based on the amount of traffic it receives, and only pay for the resources it consumes. Cost effective my use case.",
+			"The application retrieves data from the external API when the cron job runs, processing and storing it in its backend for use by other parts of the application",
 			"Architected and implemented the front-end of Apple Music's embeddable web player widget, which lets users log in and listen to full songs in the browser",
-			"Contributed extensively to MusicKit.js, a JavaScript framework that allows developers to add an Apple Music player to their web apps",
-		],
-	},
-	{
-		id: "tab-4",
-		work: "MonogoDB",
-		role: "React",
-		date: { start: "January 2021", end: "April 2022" },
-		contribution: [
-			"Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating from Northeastern",
-			"Helped solidify a brand direction for blistabloc that spans both packaging and web",
-			"Interfaced with clients on a weekly basis, providing technological expertise",
 		],
 	},
 ];
